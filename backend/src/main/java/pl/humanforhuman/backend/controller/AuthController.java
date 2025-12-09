0ded429ddf68c69393a59f2077189032b7c88d12
Ihar_Mayeuski1 <ihar.mayeuski1@epam.com>
2025-12-09 08:21:43 +0100

    Rename project, add encoder for saving Users

--- backend/src/main/java/pl/humanforhuman/backend/controller/AuthController.java
+++ backend/src/main/java/pl/humanforhuman/backend/controller/AuthController.java
@@ -3,8 +3,10 @@ package pl.humanforhuman.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.humanforhuman.backend.entity.User;
+import pl.humanforhuman.backend.repository.UserRepository;
 import pl.humanforhuman.backend.security.JwtService;
 import pl.humanforhuman.backend.service.UserService;
 import pl.humanforhuman.backend.dto.auth.LoginRequest;
@@ -17,13 +19,18 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
     public AuthController(UserService userService,
                           AuthenticationManager authenticationManager,
-                          JwtService jwtService) {
+                          JwtService jwtService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/test")
@@ -33,6 +40,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userService.register(user);
     }
 
